408d09b7a4caf9f331734dddd7042e5950e3f190
Frederik <frederikgalle@fastmail.fm>
2020-04-22 15:37:15 -0300

    Enable 'help' for builtins

--- .zshell/modules/other.zsh
+++ .zshell/modules/other.zsh
@@ -21,3 +21,11 @@ zle -N edit-command-line
 bindkey -M emacs '^XE' edit-command-line
 bindkey -M emacs '^X^E' edit-command-line
 bindkey -M vicmd 'v' edit-command-line
+
+#
+# Enable 'help' for builtin commands (like Bash)
+#
+
+unalias run-help
+autoload -Uz run-help
+alias help=run-help
