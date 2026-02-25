2be05a248bf18274b443b0eaf0e38b59bccb2d84
Frederik Galle <frederikgalle@fastmail.fm>
2026-02-25 10:40:39 +0100

    Add command to check out PR as worktree

---
+++ .zshell/commands/ghprwt.zsh
@@ -0,0 +1,18 @@
+# ghprwt - create a git worktree for a pull request
+# Usage: cpr <PR_NUMBER> [<REMOTE>]
+function ghprwt() {
+  local pr="$1"
+  local remote="${2:-origin}"
+  if [[ -z "$pr" ]]; then
+    pr=$(gh pr list --limit 100 --json number,title,updatedAt,author --template \
+      '{{range .}}{{tablerow .number .title .author.name (timeago .updatedAt)}}{{end}}' |
+      fzf --height 25% --reverse |
+      cut -f1 -d ' ') || return
+  fi
+  local branch
+  branch=$(gh pr view "$pr" --json headRefName -q .headRefName) || return
+  git fetch "$remote" "$branch"
+  git worktree add "../$branch" "$branch"
+  cd "../$branch" || return
+  echo "Switched to new worktree for PR #$pr: $branch"
+}
